18e3ff3e74ddd06d0343887b41c4ad45567d7d47
zrshishir <zrshishir@gmail.com>
2021-12-21 23:11:45 +0600

    carrier select api publisher

--- internal/app/server/handler.go
+++ internal/app/server/handler.go
@@ -3,8 +3,10 @@ package server
 import (
 	"CreateParcelApi/internal/app/model"
 	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/rs/zerolog/log"
 	"net/http"
+	"strconv"
 )
 
 func (s *server) createParcel(w http.ResponseWriter, r *http.Request) {
@@ -35,4 +37,42 @@ func (s *server) createParcel(w http.ResponseWriter, r *http.Request) {
 	}
 
 	SuccessResponse(w, http.StatusOK, "your parcel is being created withing some moments")
+}
+
+func (s *server) parcelCarrierAccept(w http.ResponseWriter, r *http.Request) {
+	var data model.CarrierRequest
+	vars := mux.Vars(r)
+
+	if err := json.NewDecoder(r.Body).Decode(&data); err != nil {
+		ErrUnprocessableEntityResponse(w, "Decode Error", err)
+		return
+	}
+	parcelID, err := strconv.Atoi(vars["id"])
+	if err != nil {
+		ErrInvalidEntityResponse(w, "Invalid Parcel ID", err)
+		return
+	}
+
+	data.ParcelID = parcelID
+	// validating input credentials for parcel request
+	if err := data.ValidateCarrierId(); err != nil {
+		ErrInvalidEntityResponse(w, "Invalid Input", err)
+		return
+	}
+
+	message, err := json.Marshal(data)
+	if err != nil {
+		log.Error().Err(err).Msg("json marshal failed")
+		ErrUnprocessableEntityResponse(w, "bad request", err)
+		return
+	}
+
+	err = s.publisherService.Push(message)
+	if err != nil {
+		log.Error().Err(err).Msg("failed to publish message")
+		ErrInternalServerResponse(w, "Failed to publish message", err)
+		return
+	}
+
+	SuccessResponse(w, http.StatusOK, "your parcel accept request is on the process")
 }
